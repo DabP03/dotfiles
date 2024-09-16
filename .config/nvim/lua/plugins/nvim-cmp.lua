481c51797a566ddc24336454e5cc8ae27f175562
DabP03 <piotrek.dabrowa@gmail.com>
2024-09-16 16:28:19 +0200

    even more nvim stuff

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -46,7 +46,7 @@ return {
                 { name = "luasnip" }, -- snippets
                 { name = "buffer" }, -- text within current buffer
                 { name = "path" }, -- file system paths
-                { name = "cmdline" },
+                -- { name = "cmdline" },
                 { name = "calc" },
             }),
             -- configure lspkind for vs-code like pictograms in completion menu
