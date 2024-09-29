f74fc9c48b6bcc5cd7e26880818bb76591086b9e
DabP03 <piotrek.dabrowa@gmail.com>
2024-09-29 23:55:00 +0200

    nvim treesitter textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,49 @@
 return {
-    'nvim-treesitter/nvim-treesitter'
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- indent = {
+            --     enable = true,
+            -- },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<leader>is", -- set to `false` to disable one of the mappings
+                    node_incremental = "<leader>ni",
+                    scope_incremental = "<leader>si",
+                    node_decremental = "<leader>nd",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = { query = "@function.outer", desc = "Select outer function"},
+                        ["if"] = { query = "@function.inner", desc = "Select inner function"},
+                        ["ac"] = { query = "@class.outer", desc = "Select outer class" },
+                        ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        ["ii"] = { query = "@conditional.inner", desc = "Select inner conditional" },
+                        ["ai"] = { query = "@conditional.outer", desc = "Select outer conditional" },
+                        ["il"] = { query = "@loop.inner", desc = "Select inner loop" },
+                        ["al"] = { query = "@loop.outer", desc = "Select outer loop" },
+                    },
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    include_surrounding_whitespace = false,
+                },
+            },
+        })
+    end
 }
