6e9a6d8d81d50e39a42568c9a8f4049554df295f
DabP03 <piotrek.dabrowa@gmail.com>
2025-06-05 15:54:29 +0200

    nvim disabled barbar

--- .config/nvim/lua/macros/general.lua
+++ .config/nvim/lua/macros/general.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_augroup("general", { clear = true })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = "general",
-	callback = function()
-		vim.cmd("BufferOrderByBufferNumber")
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+-- 	group = "general",
+-- 	callback = function()
+-- 		vim.cmd("BufferOrderByBufferNumber")
+-- 	end,
+-- })
